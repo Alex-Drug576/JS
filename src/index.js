ac85072f7e3678e78f81e4c550f6d7ce2cfaea1f
Alex-Drug576 <sasa.com630@gmail.com>
2023-02-03 16:03:54 +0200

    workingFunc

--- src/index.js
+++ src/index.js
@@ -1,28 +1,92 @@
+// const obj1 = {
+//     name: 21,
+// }
+// const obj2 = {
+//     userProfile: {
+//         age:24,
+//     }
+// }
+// const obj3 = {
+//     userProfile: {
+//         age:18,
+//         city: {
+//             address: 34,
+//         }
+//     }
+// }
+// var lock = {
+//     surname: 'gtit',
+//     name: {
+//         age: 30,
+//     },
+// };
+// var res = new Object();
+// //the function is summed from all psevdo-arrays(object) into one res
+// function allFormAll(){
+//     for(var i = 0; i < arguments.length; i++){
+//         for(var key in arguments[i]){
+//             if(typeof arguments[i][key] === 'object'){
+//                 allFormAll(arguments[i][key]);
+//             }else {
+//                 res[key] = arguments[i][key];
+//             }
+//         }
+//     }
+// }
+// allFormAll(obj1, obj2, obj3, lock);
+// console.log(res);
 
-var bookEnds = function(message) {
-	var firstLetter = message.charAt(0);
-  var lastLetter = message.charAt(message.length - 1);
-  return firstLetter.toUpperCase() + lastLetter.toUpperCase();
-};
+function calcucator(operation){
+    var res = 1;
+    for(var i = 1; i < arguments.length; i++){
+        if(typeof arguments[i] !== 'number') continue;
+        switch (operation){
+            case '+': 
+                res = arguments[i] + res;
+                break;
+            case '-': 
+                (i == 1) ? res = arguments[i] : res -= arguments[i];
+                break;
+            case '*': 
+                res = arguments[i] * res;
+                break;
+            case '/': 
+                (i == 1) ? res = arguments[i] : res /= arguments[i];  
+                break;
+        }
+    }
+    if(operation == '+') res -= 1;
+    console.log(res);
+}
+//calcucator('*',11,20);
 
-var letterSwap = function(characters) {
-	var firstLetter = characters.charAt(1);
-  var secondLetter = characters.charAt(0);
-  return firstLetter + secondLetter;
-};
+function reverseString(str){
+    var newStr = '';
+    for(var i = 1; i <= str.length; i++){
+        newStr += str[str.length - i];
+    }
+    console.log(newStr);
+}
+//reverseString('dlrow olleh');
 
-var leadingLetter = function(originalMessage) {
-	return originalMessage.charAt(Math.floor(originalMessage.length / 2));
-};
+function isCharPresent(str, symbol){
+    for( var i = 0; i < str.length; i++){
+        if(symbol == str[i]){
+            console.log(true);
+        } else{
+            console.log(false);
+        }
+    }
+}
+//isCharPresent('hello', 'h')
 
-var endSwap = function(message) {
-	return message + letterSwap(bookEnds(message));
-};
-
-var reverseString = function(string) {
-	return string.split("").reverse().join("");
-};
-
-var userMessage = prompt("enter a sentence!");
-
-alert(reverseString(leadingLetter(userMessage) + endSwap(userMessage)));
+function charIndexOf(str, symbol){
+    for(var i = 0; i < str.length; i++){
+        if(str[i] == symbol){
+            console.log(i);
+        }else{
+            console.log(-1);
+        }
+    }
+}
+charIndexOf('hello', 'h');
